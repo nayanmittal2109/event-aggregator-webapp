bfa69ed52f6c7a56dcb0431ba09663ecc00df9cc
nayanmittal2109 <agarwal.nayan2004@gmail.com>
2025-05-11 15:42:03 +0530

    Initial Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Event Aggregator</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API is running'));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+}).catch((err) => console.error(err));
